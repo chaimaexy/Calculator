58fbb4660a67692ce8fda54b8fc014df8708f863
Chaimae El Amraoui <chaimaexy@gmail.com>
2023-10-27 12:40:51 +0100

    4th commit

---
+++ app/src/main/java/ma/ensa/calculator_app/AddActivity.kt
@@ -0,0 +1,11 @@
+package ma.ensa.calculator_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add)
+    }
+}
